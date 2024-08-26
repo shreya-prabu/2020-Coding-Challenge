40de523e54a08c6763db7f91f9dd92618a822cfc
shreya-prabu <shreyaprabu00@gmail.com>
2024-08-25 20:16:43 -0700

    Update server.py
    Sort scoreboard in non-increasing order by score

--- server.py
+++ server.py
@@ -51,6 +51,9 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    # Sort scoreboard in non-increasing order by score
+    scoreboard.sort(key=lambda x: x["score"], reverse=True)
+    
     return jsonify(scoreboard=scoreboard)
 
 
